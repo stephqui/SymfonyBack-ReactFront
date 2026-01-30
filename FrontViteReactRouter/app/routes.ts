f84a5a3c3b3cc66c01f855a69697abc6b30986e8
stephqui <paradivuiz@gmail.com>
2026-01-30 16:58:58 +0100

    test ajout route

--- FrontViteReactRouter/app/routes.ts
+++ FrontViteReactRouter/app/routes.ts
@@ -1,3 +1,12 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  prefix,
+  route,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("customers", "./routes/CustomerListe.jsx"),
+] satisfies RouteConfig;
