19b80c7b00265ba92b4c7b58a238e7766a45dabb
stephqui <paradivuiz@gmail.com>
2026-02-26 17:28:29 +0100

    Encore galère des routes, il faudra rajouter la balise outlet

--- FrontViteReactRouter/app/routes.ts
+++ FrontViteReactRouter/app/routes.ts
@@ -5,8 +5,13 @@ import {
   prefix,
   route,
 } from "@react-router/dev/routes";
+import { Layout } from "./root";
 
 export default [
   index("routes/home.tsx"),
-  route("customers", "./routes/CustomerListe.jsx"),
+  //route("customers", "./routes/CustomerListe.jsx"),
+
+  ...prefix("users", [
+    layout("routes/CustomerListe.jsx", [index("routes/content/Content.tsx")]),
+  ]),
 ] satisfies RouteConfig;
